f2194238ffe9f7ada7b9db1e06fe1c3cced00223
Oge-webdeveloper <ogechiebelebe783@gamil.com>
2024-09-24 02:15:56 +0100

    I corrected the css and JS

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.nav-link');
+const xIcon = document.querySelector('.x-icon');
+const menuClass = document.querySelector('.material-symbols-outlined');
 
 console.log(menuIcon);
 
 menuIcon .addEventListener('click' , toggleOpen);
+
 function toggleOpen () {
     // menu.style.display = 'block';
+
+    if (xIcon.style.display === 'block') {
+        xIcon.style.display='none' ;
+        menuClass.style.display = 'block' ;
+    }
+    else{
+        xIcon.style.display='block' ;
+        menuClass.style.display = 'none' ;
+    }
+
     menu.classList.toggle('toggle');
 }
